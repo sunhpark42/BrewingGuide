8ca2b955340c3efb90e223e635a5d0569b833699
sunhpark42 <sunhpark42@gmail.com>
2020-08-28 21:47:18 +0900

    Add : Header Toggle 기능 추가

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     HeaderContainer, ToggleBar, Logo, MyPageIcon,
 } from './style-Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header () {
+    const [open, setOpen] = useState(false);
     return (
-        <>
-            <HeaderContainer>
-                <ToggleBar>
-                    <FontAwesomeIcon icon={faBars} />
-                </ToggleBar>
-                <Logo href="home">
-                    Brewing Guide
-                </Logo>
-                <MyPageIcon href="mypage">
-                    <FontAwesomeIcon icon={faUser} />
-                </MyPageIcon>
-            </HeaderContainer>
-        </>
+        <HeaderContainer>
+            <ToggleBar open={open} setOpen={setOpen} />
+            <Logo href="home">
+                Brewing Guide
+            </Logo>
+            <MyPageIcon href="mypage">
+                <FontAwesomeIcon icon={faUser} />
+            </MyPageIcon>
+        </HeaderContainer>
     );
 }
