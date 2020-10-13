69f4e07a32fd2c1bea78c7f8f32b2a7f9ccd6bed
sunhpark42 <sunhpark42@gmail.com>
2020-10-13 12:56:52 +0900

    Style : Toggle Style

--- src/Components/Header/style-Toggle.js
+++ src/Components/Header/style-Toggle.js
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 
 export const ToggleContainer = styled.div`
-    width: 100%;
+    @media screen and (max-width: 1440px) {
+        width: 300px;
+    }
+    @media screen and (max-width:600px) {
+        width: 100%;
+    }
+    // width: 100%;
     height: 100%;
+    box-sizing: border-box;
     background-color: #fefefe;
     transform: ${({open}) => (open ? 'translateY(0)' : 'translateY(100%)')};
     border-top: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
 export const ToggleComponents = styled.ul`
+    height: 100vh;
     background-color: #fefefe;
+
 `;
 
 export const ToggleComponent = styled.li`
@@ -21,5 +30,12 @@ export const ToggleComponent = styled.li`
     &:hover {
         background-color: #390d69;
         color: #ffffff;
+    }
+    @media screen and (max-width: 1440px) {
+        padding: 10px 50px;
+        font-size: 14px;
+    }
+    @media screen and (max-width:600px) {
+        padding : 10px 5%
     }
 `;
